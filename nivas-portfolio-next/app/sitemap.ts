a6e3997ebf24d2fe4df265413c0ec0c5566061e6
Nivas <nivasn227@gmail.com>
2026-09-10 11:32:20 +0530

    Update sitemap.ts

--- nivas-portfolio-next/app/sitemap.ts
+++ nivas-portfolio-next/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://YOUR-DOMAIN.com",
+      url: "https://nivas-portfolio-next-hazel.vercel.app",
       lastModified: new Date(),
     },
   ];
